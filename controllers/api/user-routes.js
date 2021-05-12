65a63c3583129fa80d713730c5e9291f92b7ffec
Dax Buratto <daxburatto@gmail.com>
2021-05-12 15:39:32 -0500

    finished user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -106,3 +106,53 @@ router.post('/login', (req, res) => {
     })
 })
 
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end()
+        })
+    } else {
+        res.status(404).end()
+    }
+})
+
+router.put('/:id', (req, res) => {
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({
+                message: 'No User found'
+            })
+            return
+        }
+        res.json(dbUserData)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+router.delete('/:id', (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({
+                message: 'No User found'
+            })
+            return
+        }
+        res.json(dbUserData)
+    }).catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
+})
+
+module.exports = router
