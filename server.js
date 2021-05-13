d5e8b1affa3a011411ccf972fab660dd28c11a60
Dax Buratto <daxburatto@gmail.com>
2021-05-13 16:11:22 -0500

    got loginpage working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const routes = require('./controllers')
 const session = require('express-session')
 const exphbs = require('express-handlebars')
 
@@ -11,7 +12,9 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
 const sess = {
     secret: 'Super secret secret',
-    cookie: {},
+    cookie: {
+        maxAge: 30000
+    },
     resave: false,
     saveUninitialized: true,
     store: new SequelizeStore({
@@ -21,18 +24,18 @@ const sess = {
 
 app.use(session(sess))
 
-const helpers = require('./')
+const helpers = require('./utils/helpers')
 
 const hbs = exphbs.create({helpers})
 
 app.engine('handlebars', hbs.engine)
-app.set('View engine', 'handlebars')
+app.set('view engine', 'handlebars')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended:false }))
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(require('./controllers'))
+app.use(routes)
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log('Now Listening'))
